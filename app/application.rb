6f9da866767c66e20c733398c26b303d577bc5d5
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-17 18:50:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
-      end
+    end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
