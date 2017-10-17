32f89116d4c52b39a4bf88f33ccadc9ad55c8770
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-17 18:58:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
     elsif req.match(/add/)
-      @@items.handle_search(search_term)  
+      @@items.include?(handle_search(search_term))
     else
       resp.write "Path Not Found"
     end
