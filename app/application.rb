5e7920d2f883a569f030a2d7e9ed123a88552ac7
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-17 19:06:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
           @@cart << item
           resp.write "added #{item}"
         else
-          "We don't have that item"
+          resp.write "We don't have that item"
         end
       end
     else
