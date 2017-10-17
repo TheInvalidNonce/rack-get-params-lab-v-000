b8e7370540ee15e58301a4c5996820d23fc97c7f
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-17 19:00:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,9 +18,7 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
-        @@cart.each do |item|
-        resp.write "#{item}\n"
-        end
+        @@cart.each { |item| resp.write "#{item}\n" }
       end
     elsif req.path.match(/add/)
       handle_search(search_term)
